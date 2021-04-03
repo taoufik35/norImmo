d2223087e5f2517b54130f8625f7dba78ea6114f
JoelB-E276 <joel.beauroy@outlook.fr>
2021-04-03 16:33:36 +0200

    add button

--- js/joel.js
+++ js/joel.js
@@ -8,11 +8,22 @@ p1.addEventListener("mouseout" , function(event){
 }); 
 
 // Method 2:  with text in "let"
-let rep = document.getElementById("b");
-a.addEventListener("mouseover", function(){
+let rep = document.getElementById("disp");
+detect.addEventListener("mouseover", function(){
   let text = "Réponse 2 - C'est le faux texte standard de l'imprimerie depuis les années 1500.";
   rep.innerText = text;
 });
-a.addEventListener("mouseout", function(){
+detect.addEventListener("mouseout", function(){
   rep.innerText = "";
+});
+// Method 3: input with button = answer
+var button = document.querySelector("input");
+var txt = document.getElementById("quest3");
+
+button.addEventListener("click", function () {
+  if (button.value === "Réponse") {
+    button.value = "Réponse 3 - Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire.";
+  } else {
+    button.value = "Réponse";
+  }
 });
